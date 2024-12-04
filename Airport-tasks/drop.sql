5ce38e13b836c054fe7fb907d01b4e42127fb9be
Alexander Shmakov <lexnder9@gmail.com>
2024-12-04 04:19:47 +0300

    update comments

--- Airport-tasks/drop.sql
+++ Airport-tasks/drop.sql
@@ -1,4 +1,6 @@
+------------------------------------------------------------------------------------------------------------------------
 -- Удаление таблиц
+------------------------------------------------------------------------------------------------------------------------
 DROP TABLE IF EXISTS SERVICE_CLASSES_INFO CASCADE;
 DROP TABLE IF EXISTS PASSENGER CASCADE;
 DROP TABLE IF EXISTS FLIGHT_SERVICE CASCADE;
@@ -10,17 +12,23 @@ DROP TABLE IF EXISTS BOOKING CASCADE;
 DROP TABLE IF EXISTS AIRPORT CASCADE;
 DROP TABLE IF EXISTS AIRCRAFT CASCADE;
 
+------------------------------------------------------------------------------------------------------------------------
 -- Удаление триггеров
+------------------------------------------------------------------------------------------------------------------------
 DROP TRIGGER IF EXISTS booking_insert ON BOOKING;
 DROP TRIGGER IF EXISTS booking_delete ON BOOKING;
 DROP TRIGGER IF EXISTS customer_update ON CUSTOMER;
 
+------------------------------------------------------------------------------------------------------------------------
 -- Удаление представлений
+------------------------------------------------------------------------------------------------------------------------
 DROP VIEW IF EXISTS flight_info;
 DROP VIEW IF EXISTS customer_bookings;
 DROP VIEW IF EXISTS flight_load_report;
 
+------------------------------------------------------------------------------------------------------------------------
 -- Удаление хранимых процедур
+------------------------------------------------------------------------------------------------------------------------
 DROP FUNCTION IF EXISTS register_customer(VARCHAR, VARCHAR, VARCHAR, VARCHAR);
 DROP FUNCTION IF EXISTS create_booking(INTEGER, INTEGER, INTEGER[]);
 DROP FUNCTION IF EXISTS update_flight(INTEGER, VARCHAR, TIMESTAMP, TIMESTAMP);
--- Airport-tasks/views.sql
+++ Airport-tasks/views.sql
@@ -1,19 +1,30 @@
+------------------------------------------------------------------------------------------------------------------------
 -- Представление для получения информации о рейсах с количеством забронированных мест
+------------------------------------------------------------------------------------------------------------------------
 CREATE OR REPLACE VIEW flight_info AS
-SELECT F.FLIGHT_ID, F.FLIGHT_NUMBER, F.DEPARTURE_DATE, F.ARRIVAL_DATE, FB.ECONOMY_CLASS_SEATS_BOOKED, FB.BUSINESS_CLASS_SEATS_BOOKED
+SELECT F.FLIGHT_ID,
+       F.FLIGHT_NUMBER,
+       F.DEPARTURE_DATE,
+       F.ARRIVAL_DATE,
+       FB.ECONOMY_CLASS_SEATS_BOOKED,
+       FB.BUSINESS_CLASS_SEATS_BOOKED
 FROM FLIGHT F
-JOIN FLIGHT_BOOKINGS FB ON F.FLIGHT_BOOKING_ID = FB.FLIGHT_BOOKING_ID;
+         JOIN FLIGHT_BOOKINGS FB ON F.FLIGHT_BOOKING_ID = FB.FLIGHT_BOOKING_ID;
 
+------------------------------------------------------------------------------------------------------------------------
 -- Представление для получения информации о клиентах и их бронированиях
+------------------------------------------------------------------------------------------------------------------------
 CREATE OR REPLACE VIEW customer_bookings AS
 SELECT C.CUSTOMER_ID, C.FIRST_NAME, C.LAST_NAME, B.BOOKING_ID, B.BOOKING_DATE_TIME
 FROM CUSTOMER C
-JOIN BOOKING B ON C.CUSTOMER_ID = B.CUSTOMER_ID;
+         JOIN BOOKING B ON C.CUSTOMER_ID = B.CUSTOMER_ID;
 
+------------------------------------------------------------------------------------------------------------------------
 -- Представление для получения отчетов о загруженности рейсов
+------------------------------------------------------------------------------------------------------------------------
 CREATE OR REPLACE VIEW flight_load_report AS
 SELECT F.FLIGHT_NUMBER, COUNT(B.BOOKING_ID) AS TOTAL_BOOKINGS
 FROM FLIGHT F
-LEFT JOIN BOOKING B ON F.FLIGHT_ID = B.FLIGHT_ID
+         LEFT JOIN BOOKING B ON F.FLIGHT_ID = B.FLIGHT_ID
 GROUP BY F.FLIGHT_NUMBER
 HAVING COUNT(B.BOOKING_ID) > 0;
